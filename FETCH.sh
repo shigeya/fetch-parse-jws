888fc9db89f8bb4c3b1b45e37910494e54f15748
Shigeya Suzuki <shigeya@wide.ad.jp>
2011-04-24 21:17:38 +0900

    initial: script to fetch files from the site

---
+++ FETCH.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+URL='https://spreadsheets.google.com/spreadsheet/pub?hl=en&key=0AuLjy4AyvEigdFlzejRGcGtEcldaZGw1MG9xOXdwb3c&hl=en&chrome=false&gid=0'
+wget --no-check-certificate -O t.html $URL
+
+
+if [ -x /sbin/md5 ]; then
+	MD5=/sbin/md5
+else
+	MD5=md5
+fi
+
+T_MD5=`$MD5 t.html |sed -e 's/.* = //'`
+FILE="archive/jws-${T_MD5}.html"
+if [ ! -e $FILE ]; then
+	echo New File: $FILE
+	mv t.html $FILE
+	touch fetch.log
+	echo `date`: $FILE >> fetch.log
+else
+	echo $FILE already exists.
+fi
